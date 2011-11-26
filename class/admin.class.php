8ff65a1216ebfbd0f5b5c5a288c4ed6558142b3d
Jakub <jakub.gorzelak@gmail.com>
2011-11-26 23:01:51 +0100

    Fix mysql query for creating teacher.

--- class/admin.class.php
+++ class/admin.class.php
@@ -9,14 +9,14 @@
       $teacher -> password = md5($this -> password);
       $teacher -> account_type = 'teacher';
 
-      $mysql -> query("INSERT INTO '$db_name'.'users'
+      $mysql -> query("INSERT INTO $db_name.users
         (login, password, first_name, last_name, mail, account_type)
         VALUES ('$teacher->login',
                 '$teacher->password',
                 '$teacher->first_name',
                 '$teacher->last_name',
                 '$teacher->mail',
-                '$teacher->account_type)");
+                '$teacher->account_type')");
     }
 
   }
