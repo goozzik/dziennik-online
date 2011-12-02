4a50ebf9748fad4868baf9bc76c00a711c112c0c
Jakub <jakub.gorzelak@gmail.com>
2011-12-03 00:01:23 +0100

    Add beforeFilter for Description

--- app/Controller/DescriptionsController.php
+++ app/Controller/DescriptionsController.php
@@ -3,14 +3,19 @@ class DescriptionsController extends AppController {
   public $name = 'Descriptions';
 
   function beforeFilter() {
-    #
+    $this->loadModel('Subject');
+    $subject = $this->Subject->findByTeacherIdAndId($this->Session->read('Auth.User.class_id'), $this->request->data['Description']['subject_id']);
+    if(!($this->Session->read('Auth.User.teacher') && empty($subject))) {
+      $this->Session->setFlash('Brak dostępu.', 'flash_error');
+      $this->redirect($this->referer());
+    }
   }
 
   function create() {
     if ($this->request->is('post')) {
       $this->Description->create();
       if ($this->Description->save($this->request->data)) {
-        $this->Session->setFlash('Dodano nową ocenę.', 'flash_success');
+        $this->Session->setFlash('Dodano ocenę.', 'flash_success');
         $this->redirect($this->referer());
       }
     }
