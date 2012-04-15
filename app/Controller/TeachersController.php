11a6e29fce4bb9a024410885db567e9d5c761897
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-04-15 12:54:30 +0200

    Refactor controller and add some filters.

--- app/Controller/TeachersController.php
+++ app/Controller/TeachersController.php
@@ -1,17 +1,31 @@
 <?php
 class TeachersController extends AppController {
+
   public $name = 'Teachers';
 
   function beforeFilter() {
-  	if ($this->params['teacher']) {
-  	  $this->isTeacherFilter();	
-  	  if ($this->action == 'teacher_edit') {
-		$this->isOwningClassFilter();
-  	  }
-  	}
+    parent::beforeFilter();
+    if ($this->action == 'teacher_edit') {
+      $this->classAuth();
+      $this->semesterAuth();
+    }
   }
 
-  function _refreshAuth($field = '', $value = '') {
+  private function classAuth() {
+    if (!$this->Teacher->SchoolClass->findByIdAndTeacherId($this->request->data['Teacher']['class_id'], $this->currentUser('id'))) {
+      $this->Session->setFlash('Brak dostępu.', 'flash_error');
+      $this->redirect($this->referer());
+    }
+  }
+
+  private function semesterAuth() {
+    if (!$this->Teacher->Semester->findByIdAndTeacherId($this->request->data['Teacher']['semester_id'], $this->currentUser('id'))) {
+      $this->Session->setFlash('Brak dostępu.', 'flash_error');
+      $this->redirect($this->referer());
+    }
+  }
+
+  /*function _refreshAuth($field = '', $value = '') {
     if (!empty($field) && !empty($value)) { 
       $this->Session->write($this->Auth->sessionKey .'.'. $field, $value);
     } else {
@@ -21,26 +35,15 @@ class TeachersController extends AppController {
         $this->Auth->login(ClassRegistry::init('User')->findById($this->Auth->user('id')));
       }
     }
-  }
-
-  function isOwningClassFilter()
-  {
-  	$class = $this->Teacher->SchoolClass->findById($this->request->data['Teacher']['class_id']);
-    if($class['SchoolClass']['teacher_id'] != $this->currentUser('id')) {
-      $this->Session->setFlash('Brak dostępu.', 'flash_error');
-      $this->redirect($this->referer());
-    }
-  }
+  }*/
 
   function teacher_edit() {
     $this->Teacher->id = $this->currentUser('id');
-    if ($this->request->is('post')) {
-      $this->Teacher->save($this->request->data);
-      // This should also update other fields in session, TODO
-      $this->Session->write('Auth.User.class_id', $this->request->data['Teacher']['class_id']); 
-      $this->Session->write('Auth.User.semester_id', $this->request->data['Teacher']['semester_id']); 
+    if ($this->Teacher->save($this->request->data)) {
       $this->Session->setFlash('Zapisano pomyślnie.', 'flash_success');
-      $this->redirect(array('controller' => 'school_classes', 'action' => 'index'));
+      $this->redirect('/teacher/school_classes');
     }
   }
+
 }
+?>
