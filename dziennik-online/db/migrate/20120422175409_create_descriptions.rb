d3149fd92ddaddcabf510001f0d51177166de4d0
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-04-22 19:55:47 +0200

    Add create_descriptions migration.

---
+++ dziennik-online/db/migrate/20120422175409_create_descriptions.rb
@@ -0,0 +1,19 @@
+class CreateDescriptions < ActiveRecord::Migration
+
+  def up
+    create_table :descriptions do |t|
+      t.integer :subject_id
+      t.integer :semester_id
+      t.string :type
+      t.integer :colour
+      t.string :description
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :descriptions
+  end
+
+end
