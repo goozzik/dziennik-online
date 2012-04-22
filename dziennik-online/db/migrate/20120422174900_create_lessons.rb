b7726369aaa00df1b4b773f755f0226530e0f77b
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-04-22 19:50:50 +0200

    Add create_lessons migration.

---
+++ dziennik-online/db/migrate/20120422174900_create_lessons.rb
@@ -0,0 +1,17 @@
+class CreateLessons < ActiveRecord::Migration
+
+  def up
+    create_table :lessons do |t|
+      t.integer :subject_id
+      t.integer :time_table_id
+      t.integer :lesson_number
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :lessons
+  end
+
+end
