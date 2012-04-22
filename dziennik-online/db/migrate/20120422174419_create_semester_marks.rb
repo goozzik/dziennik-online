d9f5139abbea6a738302b693481b208f28e496ed
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-04-22 19:46:21 +0200

    Add create_semester_marks migration.

---
+++ dziennik-online/db/migrate/20120422174419_create_semester_marks.rb
@@ -0,0 +1,19 @@
+class CreateSemesterMarks < ActiveRecord::Migration
+
+  def up
+    create_table :semester_marks do |t|
+      t.integer :student_id
+      t.integer :class_id
+      t.integer :semester_id
+      t.integer :subject_id
+      t.integer :mark
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :semester_marks
+  end
+
+end
