4fe87c0da9b4f0c777ea6a5211bb832d95fdecfe
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-04-22 19:53:30 +0200

    Add create_documents migration.

---
+++ dziennik-online/db/migrate/20120422175106_create_documents.rb
@@ -0,0 +1,21 @@
+class CreateDocuments < ActiveRecord::Migration
+
+  def up
+    create_table :documents do |t|
+      t.integer :class_id
+      t.integer :semester_id
+      t.string :name
+      t.string :description
+      t.string :path
+      t.string :type
+      t.integer :size
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :documents
+  end
+
+end
