726951cb9bd28c79e0521abd67e8130c7f775603
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-04-25 15:08:21 +0200

    Setup default order for semesters and school classes.

--- dziennik-online/app/models/semester.rb
+++ dziennik-online/app/models/semester.rb
@@ -3,6 +3,7 @@ class Semester < ActiveRecord::Base
   belongs_to :teacher
   belongs_to :school_class
   attr_accessible :semester, :active, :school_class_id
+  default_scope :order => 'created_at ASC'
 
   before_create :inherit_from_school_class, :unactive_old_semester, :set_active, :set_semester_number
   after_create :set_teacher_current_semester_and_class
