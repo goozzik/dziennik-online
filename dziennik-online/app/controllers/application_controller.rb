6df56d56ed6cdd9d7899f876d355a424707d3a3f
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-04-25 20:35:51 +0200

    Move filter methods to application_controller.rb part #1 story #115

--- dziennik-online/app/controllers/application_controller.rb
+++ dziennik-online/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class ApplicationController < ActionController::Base
 
   protect_from_forgery
@@ -11,6 +12,22 @@ class ApplicationController < ActionController::Base
     # Overwriting the sign_in redirect path method
     def after_sign_in_path_for(resource_or_scope)
       root_path
+    end
+
+  protected
+
+    def is_teacher_have_active_class?
+      unless current_teacher.school_class
+        flash[:notice] = "Najpierw dodaj klasę."
+        redirect_to(:controller => "teacher/school_classes", :action => "index")
+      end
+    end
+
+    def is_teacher_have_active_semester?
+      unless current_teacher.semester
+        flash[:notice] = "Najpierw dodaj semestr."
+        redirect_to(:controller => "teacher/school_classes", :action => "index")
+      end
     end
 
 end
--- dziennik-online/app/controllers/teacher/subjects_controller.rb
+++ dziennik-online/app/controllers/teacher/subjects_controller.rb
@@ -19,20 +19,4 @@ class Teacher::SubjectsController < ApplicationController
     @students = current_teacher.school_class.students
   end
 
-  private
-
-    def is_teacher_have_active_class?
-      unless current_teacher.school_class
-        flash[:notice] = "Najpierw dodaj klasę."
-        redirect_to(:controller => "teacher/school_classes", :action => "index")
-      end
-    end
-
-    def is_teacher_have_active_semester?
-      unless current_teacher.semester
-        flash[:notice] = "Najpierw dodaj semestr."
-        redirect_to(:controller => "teacher/school_classes", :action => "index")
-      end
-    end
-
 end
