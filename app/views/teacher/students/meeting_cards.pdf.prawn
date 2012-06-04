93f189955c521f4a97307f65c70ae1dda192dd75
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-06-04 20:58:57 +0200

    Fix typo.

--- app/views/teacher/students/meeting_cards.pdf.prawn
+++ app/views/teacher/students/meeting_cards.pdf.prawn
@@ -1,6 +1,6 @@
 prawn_document do |pdf|
   @students.each do |student|
-    table = [[{content: "#{student.first_name} #{student.last_name} - #{student.school_class_semester.fullname}", colspan:3}]]
+    table = [[{content: "#{student.first_name} #{student.last_name} - #{student.school_class.semester.fullname}", colspan:3}]]
     table << [{content: "Przedmiot", colspan:2}, "Oceny"]
     @subjects.each do |subject|
       table << [{content: subject.name, colspan:2}, student.list_current_marks_by_subject_id(subject.id)]
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,17 @@ SubjectTemplate.create([{:name => "Administracja sieci"}, {:name => "Aplikajce b
                 {:name => "Wiedza o kulturze"}, {:name => "Wiedza o społeczeństwie"}])
 
 school = FactoryGirl.create(:school)
-semester = FactoryGirl.create(:semester, :school_id => school.id)
-admin = FactoryGirl.create(:admin, :school_id => school.id)
-teacher = FactoryGirl.create(:teacher, :school_id => school.id)
-school_class = FactoryGirl.create(:school_class, :teacher_id => teacher.id)
-director = FactoryGirl.create(:director, :school_id => school.id)
+semester = FactoryGirl.create(:semester, school_id:school.id, start_year:2011, end_year:2012, semester:2)
+admin = FactoryGirl.create(:admin, school_id:school.id, username:"administrator")
+
+1.upto(4) do |a|
+  teacher = FactoryGirl.create(:teacher, school_id:school.id, username:"nauczyciel#{a}")
+  school_class = FactoryGirl.create(:school_class, teacher_id:teacher.id, yearbook:2016-a)
+  1.upto(21).each do |b|
+    student = FactoryGirl.create(:student, school_class_id:school_class.id, username:"Testiusz#{b}")
+    ['2012-03-05', '2012-04-02', '2012-04-09', '2012-04-16', '2012-04-30', '2012-05-07', '2012-05-14', '2012-05-21', '2012-06-04', '2012-06-11', '2012-06-18', '2012-06-25'].each do |c|
+      FactoryGirl.create(:absence, student_id:student.id, date:c, required:30, justified:b+1, unexcused:1)
+    end
+  end
+end
+director = FactoryGirl.create(:director, school_id:school.id, username:"dyrektor")
