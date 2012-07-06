11a2343691e145c4ae8869e24fe3a8b2a441ab19
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-07-06 14:18:46 +0200

    Add tests for director absences.

--- spec/acceptance/director_absences_feature_spec.rb
+++ spec/acceptance/director_absences_feature_spec.rb
@@ -8,12 +8,53 @@ feature "Director absences" do
       FactoryGirl.create(:school)
       FactoryGirl.create(:semester, :school_id => School.last.id)
       FactoryGirl.create(:director, :school_id => School.last.id)
-      login('director')
+      FactoryGirl.create(:teacher, :school_id => School.last.id)
+      FactoryGirl.create(:school_class, :teacher_id => Teacher.last.id)
+      login("director")
+      click_link "Klasy"
     end
 
-    scenario 'there is no active school class in school' do
-      click_link "Klasy"
-      assert_info_box "Szkoła nie ma aktywnych klas."
+    scenario "there is no students in school class" do
+      click_link "Bieżąca frekwencja"
+      assert_alert_box "Wychowawca klasy nie dodał jeszcze uczniów."
+    end
+
+    scenario "when there is data for school class" do
+      load_subjects_for_report
+      load_data_for_school_class_semester_report
+      click_link "Bieżąca frekwencja"
+      page.should have_xpath "//tr[3]/td[3][contains(text(), '30')]"
+      page.should have_xpath "//tr[3]/td[4][contains(text(), '20')]"
+      page.should have_xpath "//tr[3]/td[5][contains(text(), '0')]"
+      page.should have_xpath "//tr[3]/td[6][contains(text(), '5')]"
+      page.should have_xpath "//tr[6]/th[2][contains(text(), '33.33')]"
+      page.should have_xpath "//tr[6]/th[3][contains(text(), '90')]"
+      page.should have_xpath "//tr[6]/th[4][contains(text(), '60')]"
+      page.should have_xpath "//tr[6]/th[5][contains(text(), '0')]"
+      page.should have_xpath "//tr[6]/th[6][contains(text(), '15')]"
+    end
+
+    context "month navigation" do
+      before do
+        FactoryGirl.create(:student, :school_class_id => SchoolClass.last.id)
+        click_link "Bieżąca frekwencja"
+      end
+
+      scenario "follow next month link" do
+        month = Chronic.parse('monday this month')
+        next_month = Chronic.parse('monday next month', :now => month)
+        find(:xpath, "//a[@href='/director/school_classes/#{SchoolClass.last.id}/absences?date=#{next_month.strftime("%Y-%m-%d")}']").click
+        page.should have_content "Frekwencja"
+        page.should have_content "#{I18n.t(next_month.strftime("%B"))} #{next_month.year}"
+      end
+
+      scenario "follow previous month link" do
+        month = Chronic.parse('monday this month')
+        previous_month = Chronic.parse('monday last month', :now => month)
+        find(:xpath, "//a[@href='/director/school_classes/#{SchoolClass.last.id}/absences?date=#{previous_month.strftime("%Y-%m-%d")}']").click
+        page.should have_content "Frekwencja"
+        page.should have_content "#{I18n.t(previous_month.strftime("%B"))} #{previous_month.year}"
+      end
     end
 
   end
