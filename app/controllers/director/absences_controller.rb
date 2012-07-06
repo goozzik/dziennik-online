45a2380fbd43d3d6a40591206fca74ea01522992
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-07-06 13:50:18 +0200

    Move before filter to one line.

--- app/controllers/director/absences_controller.rb
+++ app/controllers/director/absences_controller.rb
@@ -1,7 +1,6 @@
 class Director::AbsencesController < ApplicationController
 
-  before_filter :authenticate_director!
-  before_filter :director_school_class_has_students?
+  before_filter :authenticate_director!, :director_school_class_has_students?
 
   def index
     @month = Chronic.parse('monday this month', :now => (params[:date].nil? ? Time.now : Time.parse(params[:date])))
@@ -9,6 +8,5 @@ class Director::AbsencesController < ApplicationController
     @previous_month = Chronic.parse('monday last month', :now => @month)
     @next_month = Chronic.parse('monday next month', :now => @month)
   end
-
 
 end
