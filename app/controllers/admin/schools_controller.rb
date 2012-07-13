f4b4b633b4da878a01ceefbc2b3169ceaa37ff13
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-07-13 16:03:53 +0200

    Refactor admin/schools controller and views.

--- app/controllers/admin/schools_controller.rb
+++ app/controllers/admin/schools_controller.rb
@@ -3,6 +3,8 @@ class Admin::SchoolsController < ApplicationController
   before_filter :authenticate_admin!
 
   def show
+    @semesters = current_admin.semesters
+    @semester = Semester.new
   end
 
 end
