cac453cd6fdcaae15a7035337bcee3461d70546f
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-10-14 10:17:04 +0200

    Fix months indicies and director absences index bug.

--- app/controllers/director/absences_controller.rb
+++ app/controllers/director/absences_controller.rb
@@ -3,10 +3,9 @@ class Director::AbsencesController < ApplicationController
   before_filter :authenticate_director!, :director_school_class_has_students?
 
   def index
-    @month = Absence.parse_first_monday(params[:date].nil? ? Time.now : Time.parse(params[:date]))
-    @weeks = Absence.get_weeks_from_month(@month)
-    @previous_month = Absence.parse_monday_last_month('monday last month', :now => @month)
-    @next_month = Absence.parse_monday_next_month('monday next month', :now => @month)
+    @month = params[:month].nil? ? Time.now.month : params[:month]
+    @previous_month = Absence.previous_month(@month)
+    @next_month = Absence.next_month(@month)
   end
 
 end
