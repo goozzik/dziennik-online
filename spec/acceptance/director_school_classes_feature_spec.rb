16c745eb83d97fd65156c36ac76663b2cb8f1369
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2012-11-04 11:26:45 +0100

    Fix code syntax. Story #245

--- spec/acceptance/director_school_classes_feature_spec.rb
+++ spec/acceptance/director_school_classes_feature_spec.rb
@@ -8,7 +8,7 @@ feature "Director school classes" do
       FactoryGirl.create(:school)
       FactoryGirl.create(:profile_template, school_id: School.last.id)
       load_semester
-      FactoryGirl.create(:director, :school_id => School.last.id)
+      FactoryGirl.create(:director, school_id: School.last.id)
       login('director')
     end
 
@@ -18,16 +18,16 @@ feature "Director school classes" do
     end
 
     scenario "when there is active school class in school" do
-      FactoryGirl.create(:teacher, :school_id => School.last.id)
-      FactoryGirl.create(:school_class, :teacher_id => Teacher.last.id, profile: "Technik awionik", yearbook: Semester.last.end_year)
+      FactoryGirl.create(:teacher, school_id: School.last.id)
+      FactoryGirl.create(:school_class, teacher_id: Teacher.last.id, profile: "Technik awionik", yearbook: Semester.last.end_year)
       click_link "Klasy"
       page.should have_content "4 G Technik awionik #{Semester.last.end_year}"
     end
 
     context "follow 'Bieżąca frekwencja'" do
       before do
-        FactoryGirl.create(:teacher, :school_id => School.last.id)
-        FactoryGirl.create(:school_class, :teacher_id => Teacher.last.id, profile: "Technik awionik", yearbook: Semester.last.end_year)
+        FactoryGirl.create(:teacher, school_id: School.last.id)
+        FactoryGirl.create(:school_class, teacher_id: Teacher.last.id, profile: "Technik awionik", yearbook: Semester.last.end_year)
         click_link "Klasy"
       end
 
@@ -37,7 +37,7 @@ feature "Director school classes" do
       end
 
       scenario "when there is student" do
-        FactoryGirl.create(:student, :school_class_id => SchoolClass.last.id)
+        FactoryGirl.create(:student, school_class_id: SchoolClass.last.id)
         click_link "Bieżąca frekwencja"
         page.should have_content "Frekwencja klasy 4 G"
       end
@@ -46,14 +46,14 @@ feature "Director school classes" do
 
     context "follow 'Bieżące Oceny'" do
       before do
-        FactoryGirl.create(:teacher, :school_id => School.last.id)
-        FactoryGirl.create(:school_class, profile: "Technik awionik", :teacher_id => Teacher.last.id)
+        FactoryGirl.create(:teacher, school_id: School.last.id)
+        FactoryGirl.create(:school_class, profile: "Technik awionik", teacher_id: Teacher.last.id)
         click_link "Klasy"
       end
 
       context "when there is student" do
         before do
-          FactoryGirl.create(:student, :school_class_id => SchoolClass.last.id)
+          FactoryGirl.create(:student, school_class_id: SchoolClass.last.id)
         end
 
         scenario "and no subject" do
@@ -63,7 +63,7 @@ feature "Director school classes" do
 
         scenario "and subject" do
           FactoryGirl.create(:subject_template)
-          FactoryGirl.create(:subject, subject_template_id:SubjectTemplate.last.id, :school_class_id => SchoolClass.last.id)
+          FactoryGirl.create(:subject, subject_template_id:SubjectTemplate.last.id, school_class_id: SchoolClass.last.id)
           click_link "Bieżące oceny"
           page.should have_content "Oceny klasy 4 G"
         end
