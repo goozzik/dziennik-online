db9f883871fda65a86e200e4150da5c321ab5d5d
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2013-01-06 16:37:51 +0100

    Add behavior mark to student. story #257

--- app/controllers/student/marks_controller.rb
+++ app/controllers/student/marks_controller.rb
@@ -3,8 +3,6 @@ class Student::MarksController < ApplicationController
   before_filter :authenticate_student!
 
   def index
-    @student = current_student
-    @subjects = current_student.subjects
   end
 
 end
