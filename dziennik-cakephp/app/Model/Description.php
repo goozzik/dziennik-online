212780405b2ba099ac2701ecc9ede42912eecc73
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2013-11-30 20:53:58 +0100

    Remove old cakephp version.

--- dziennik-cakephp/app/Model/Description.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Description extends AppModel {
-	
-  public $name = 'Description';
-  public $belongsTo = array('Semester', 'Subject');
-  public $hasMany = 'Mark';
-
-}
--- dziennik-cakephp/app/Model/Superadmin.php
+++
@@ -1,7 +0,0 @@
-<?php
-App::import('Model','User');
-class Superadmin extends User {
-  public $name = 'Superadmin';
-  public $useTable = 'users';
-  public $primaryKey = 'id';
-}
