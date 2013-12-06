4c0d4ad78fd9f278ae0082546329cdade3b54e6c
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2013-12-06 14:17:08 +0100

    Move student tests to sutdent dir and make them work.

--- spec/acceptance/student/student_reports_feature_spec.rb
+++ spec/acceptance/student/student_reports_feature_spec.rb
@@ -8,13 +8,14 @@ feature "Student reports" do
     before do
       FactoryGirl.create(:school)
       load_subject_templates
-      FactoryGirl.create(:teacher, school_id:School.last.id)
+      FactoryGirl.create(:teacher, school: School.first)
       load_semester
-      FactoryGirl.create(:school_class, profile: "Technik awionik", teacher_id:Teacher.last.id)
+      FactoryGirl.create(:school_class, profile: "Technik awionik",
+                         teacher_id: Teacher.last.id)
     end
 
     scenario "when there is data for semester" do
-      load_data_for_school_class_semester_report(SchoolClass.last)
+      load_data_for_school_class_semester_report(SchoolClass.first)
       login "student"
       click_link "Raporty"
       page.should have_xpath "//tr[3]/td[2][contains(text(), '33.33')]"
@@ -48,10 +49,10 @@ feature "Student reports" do
 
     scenario "when there is data for year" do
       load_second_semester
-      load_data_for_school_class_year_report(SchoolClass.last)
+      load_data_for_school_class_year_report(SchoolClass.first)
       login "student"
       click_link "Raporty"
-      page.should have_xpath "//h2[contains(text(), 'Podsumowanie roku szkolnego #{School.last.school_year}')]"
+      page.should have_xpath "//h2[contains(text(), 'Podsumowanie roku szkolnego #{School.first.school_year}')]"
       page.should have_xpath "//table[3]/tr[3]/td[2][contains(text(), '33.33')]"
       page.should have_xpath "//table[3]/tr[3]/td[3][contains(text(), '180')]"
       page.should have_xpath "//table[3]/tr[3]/td[4][contains(text(), '120')]"
@@ -72,17 +73,17 @@ feature "Student reports" do
       page.should have_xpath "//table[3]/tr[4]/td[5][contains(text(), '0')]"
       page.should have_xpath "//table[3]/tr[4]/td[6][contains(text(), '90')]"
       page.should have_xpath "//table[3]/tr[4]/td[7][contains(text(), '3.5')]"
-      page.should have_xpath "//table[3]/tr[4]/td[8][contains(text(), '5')]"
-      page.should have_xpath "//table[3]/tr[4]/td[9][contains(text(), '5')]"
-      page.should have_xpath "//table[3]/tr[4]/td[10][contains(text(), '5')]"
-      page.should have_xpath "//table[3]/tr[4]/td[11][contains(text(), '5')]"
-      page.should have_xpath "//table[3]/tr[4]/td[12][contains(text(), '5')]"
-      page.should have_xpath "//table[3]/tr[4]/td[13][contains(text(), '5')]"
+      page.should have_xpath "//table[3]/tr[4]/td[8][contains(text(), '3')]"
+      page.should have_xpath "//table[3]/tr[4]/td[9][contains(text(), '3')]"
+      page.should have_xpath "//table[3]/tr[4]/td[10][contains(text(), '3')]"
+      page.should have_xpath "//table[3]/tr[4]/td[11][contains(text(), '3')]"
+      page.should have_xpath "//table[3]/tr[4]/td[12][contains(text(), '3')]"
+      page.should have_xpath "//table[3]/tr[4]/td[13][contains(text(), '3')]"
       page.should have_xpath "//table[3]/tr[4]/td[14][contains(text(), '0')]"
     end
 
     scenario "when there is no data" do
-      FactoryGirl.create(:student, school_class_id:SchoolClass.last.id)
+      FactoryGirl.create(:student, school_class: SchoolClass.first)
       login "student"
       click_link "Raporty"
       page.should have_xpath "//tr[3]/td[2][contains(text(), '--')]"
@@ -99,7 +100,6 @@ feature "Student reports" do
       page.should have_xpath "//tr[3]/td[13][contains(text(), '0')]"
       page.should have_xpath "//tr[3]/td[14][contains(text(), '0')]"
     end
-
   end
 
 end
